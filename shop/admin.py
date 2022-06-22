91464365a420d1900bb33d19e076066c87050832
chetan6780 <crp20802@gmail.com>
2022-06-22 23:22:38 +0530

    Changed admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,22 @@
 import imp
+from unicodedata import name
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+admin.site.site_header = 'Shopxclusive'
+admin.site.site_title = 'Shopxclusive'
+# admin.site.index_title = 'Manage ABC buying website'
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'desc', 'image', 'seller_name')
+    search_fields = ('name',)
+    actions = ('set_price_to_zero',)
+    list_editable = ('price', 'desc')
+
+    def set_price_to_zero(self, request, queryset):
+        queryset.update(price=0)
+
+
+admin.site.register(Product, ProductAdmin)
