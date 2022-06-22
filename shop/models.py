6c776a2139c000858e0ae5e051c958dd701c32f9
chetan6780 <crp20802@gmail.com>
2022-06-22 22:57:23 +0530

    class models created

--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Product(models.Model):
-    seller_name = models.ForeignKey(User, on_delete=models.CASCADE,default=1)
+    seller_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     desc = models.CharField(max_length=255)
@@ -11,3 +12,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:products')
